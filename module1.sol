f11609c3e4ad071d17022767adad35d616ca6c71
meanuragbharti <101868239+meanuragbharti@users.noreply.github.com>
2023-08-29 10:21:56 +0530

    Update module1.sol

--- module1.sol
+++ module1.sol
@@ -1,24 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
-contract Randoms {
-    uint256 public b = 8;
+contract ErrorHandlingExample {
+    address public owner;
+    uint256 public value;
 
-    function testAssert(uint256 num) public pure {
-        assert(num != 0);
+    constructor() {
+        owner = msg.sender;
+        value = 100;
     }
 
-    function divide(uint256 numr, uint256 denm) public pure returns (uint256) {
-        if(_numr<_denm){
-           
-            revert("please provide numerator greater than denomenator");
-            
-        }
-        return _numr/_denm;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 
-    function mult(uint256 a) public view returns (uint256) {
-        require(a > 0, "Value of 'a' must be greater than zero");
-        return a * b;
+    function setValue(uint256 _newValue) external onlyOwner {
+        require(_newValue <= 1000, "Value cannot exceed 1000");
+        value = _newValue;
+    }
+
+    function assertExample(uint256 x, uint256 y) external pure returns (uint256) {
+        // Assert that x is greater than y, otherwise, the transaction will revert.
+        assert(x > y);
+        return x - y;
+    }
+
+    function revertExample(uint256 divisor) external pure returns (uint256) {
+        // Revert if divisor is 0, preventing division by zero.
+        require(divisor != 0, "Cannot divide by zero");
+        return 100 / divisor;
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+
+        // Revert the transaction if the balance is zero.
+        require(balance > 0, "Contract balance is empty");
+
+        // Transfer the contract balance to the owner.
+        (bool success, ) = owner.call{value: balance}("");
+        require(success, "Transfer failed");
     }
 }
